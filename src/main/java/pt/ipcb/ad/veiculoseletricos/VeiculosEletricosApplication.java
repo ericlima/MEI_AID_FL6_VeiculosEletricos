8f513183915126631636085b328778d77e5d8e41
Eric Lima <eric.george.lima@gmail.com>
2025-04-10 06:36:43 +0100

    initial

---
+++ src/main/java/pt/ipcb/ad/veiculoseletricos/VeiculosEletricosApplication.java
@@ -0,0 +1,13 @@
+package pt.ipcb.ad.veiculoseletricos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeiculosEletricosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeiculosEletricosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pt/ipcb/ad/veiculoseletricos/VeiculosEletricosApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ipcb.ad.veiculoseletricos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeiculosEletricosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
